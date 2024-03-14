2f8bff22d6069777c099a6b8ebfb5dd5a052764a
Vikas Surera <vikassurera@gmail.com>
2024-03-15 02:49:15 +0530

    build issue fixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import './App.css';
-import CvPage from "./CvPage";
 import HomePage from "./Pages/HomePage";
 
 function App() {
--- src/components/Portoflio/AboutCard.tsx
+++ src/components/Portoflio/AboutCard.tsx
@@ -1,18 +1,15 @@
 import React from "react";
 import CardContainer from "./CardContainer";
 
-interface AboutCardProps {
-
-}
-
-const AboutCard: React.FC<AboutCardProps> = (props) => {
+const AboutCard: React.FC = () => {
     return <CardContainer id={'about-card'}>
         <div>
             <h5>
                 {'<About />'}
             </h5>
             <div>
-                Hey! I'm vikas, a full stack developer from IIT Kharagpur. My expertise lies in creating web and mobile applications.
+                Hey! I'm vikas, a full stack developer from IIT Kharagpur. My expertise lies in creating web and mobile
+                applications.
             </div>
             <div className={'mt-2'}>
                 Exploring new tech to create better solutions.
