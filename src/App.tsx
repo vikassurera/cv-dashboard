1205b6cfc1f01c1bd0ba6f11493664a0c590e783
Vikas Surera <vikassurera@gmail.com>
2024-03-20 17:09:43 +0530

    added hash router

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import {HashRouter as Router, Route, Routes} from 'react-router-dom';
 import CvPage from "./CvPage";
 import HomePage from "./Pages/HomePage";
 
